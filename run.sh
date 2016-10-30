a11a34d8594fdf28a1886c811cd6d2cae3e6137c
Emory Merryman <emory.merryman@gmail.com>
2016-10-30 14:59:29 +0000

    fixed pull error

--- run.sh
+++ run.sh
@@ -40,6 +40,7 @@ project(){
                     MINOR=$(git rev-parse --abbrev-ref HEAD | cut -f 3 -d "/") &&
                     NEXT=$(printf %05d $((${MINOR}+1))) &&
                     (! git fetch upstream milestones/${MAJOR}/${NEXT} > /dev/null 2>&1 || (echo "Ineligible for a minor milestone upgrade." && exit 67)) &&
+                    git fetch upstream milestones/${MAJOR}/${MINOR} &&
                     git checkout upstream/milestone/${MAJOR}/${MINOR} &&
                     git checkout -b milestones/${MAJOR}/${NEXT} &&
                     git push authority milestones/${MAJOR}/${NEXT}
